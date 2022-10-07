6f9b480d4e0a0ebebed16e60be6c3b0e5da1a40c
Cavall <antoniodonizethe@hotmail.com>
2022-10-07 13:52:09 -0400

    incluso botao alterar e url para aciona-lo

--- formulario/server.js
+++ formulario/server.js
@@ -5,8 +5,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/usuarios', (req, resp) => {
-    console,console.log(req.body);
-    resp.send('<h1>PARABENS</h1>')
+    console.log(req.body);
+    resp.send('<h1>PARABENS USUARIO INCLUIDO</h1>')
 })
+
+//incluindo codigo para compatibilizar uso de alterar dados pelo button alterar
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    resp.send('<h1>PARABENS USUARIO ALTERADO</h1>')
+})
+
+
 
 app.listen(3003);
